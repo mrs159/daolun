77b65d310df2d2de704891802690abf54196ec94
mrs159 <34955426+mrs159@users.noreply.github.com>
2017-12-30 15:20:31 +0800

    Add files via upload

---
+++ xiangling/xiangling.ino
@@ -0,0 +1,173 @@
+#define pushButton  6
+#define buzzer_pin 8 //定义蜂鸣器驱动引脚
+#define buzzer_fre 600 //定义蜂鸣器输出频率
+#define NOTE_D0 -1
+#define NOTE_D1 294
+#define NOTE_D2 330
+#define NOTE_D3 350
+#define NOTE_D4 393
+#define NOTE_D5 441
+#define NOTE_D6 495
+#define NOTE_D7 556
+
+#define NOTE_DL1 147
+#define NOTE_DL2 165
+#define NOTE_DL3 175
+#define NOTE_DL4 196
+#define NOTE_DL5 221
+#define NOTE_DL6 248
+#define NOTE_DL7 278
+
+#define NOTE_DH1 589
+#define NOTE_DH2 661
+#define NOTE_DH3 700
+#define NOTE_DH4 786
+#define NOTE_DH5 882
+#define NOTE_DH6 990
+#define NOTE_DH7 112
+//以上部分是定义是把每个音符和频率值对应起来，其实不用打这么多，但是都打上了，后面可以随意编写D调的各种歌，我这里用NOTE_D+数字表示音符，NOTE_DH+数字表示上面有点的那种音符，NOTE_DL+数字表示下面有点的那种音符。这样后面写起来比较好识别。
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.25
+#define SIXTEENTH 0.625
+//这部分是用英文对应了拍子，这样后面也比较好看
+int tune[] = 
+{
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,
+
+  NOTE_D1,NOTE_D1,NOTE_D3,
+  NOTE_D1,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D7,NOTE_D7,NOTE_D5,NOTE_D3,
+  NOTE_D5,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D3,NOTE_D3,NOTE_D1,NOTE_DL6,
+  NOTE_D1,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_DH1,NOTE_D0,NOTE_D7,NOTE_D5,
+  NOTE_D6,
+};//这部分就是整首曲子的音符部分，用了一个序列定义为tune，整数
+
+float duration[]=
+{
+  1,1,0.5,0.5,1,
+  0.5,0.5,0.5,0.5,1,0.5,0.5,
+  0.5,1,0.5,1,0.5,0.5,
+  0.5,0.5,0.5,0.5,1,1,
+
+  1,1,1+1,
+  0.5,1,1+0.5,1,
+  1,1,0.5,0.5,1,
+  0.5,1,1+0.5,1,
+  0.5,0.5,0.5,0.5,1+1,
+  0.5,1,1+0.5,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,1,0.5,1+1,
+  0.5,1,0.5,1+1,
+  1+1,0.5,0.5,1,
+  1+1+1+1
+};//这部分是整首曲子的接拍部分，也定义个序列duration，浮点（数组的个数和前面音符的个数是一样的，一一对应么）
+int length;//这里定义一个变量，后面用来表示共有多少个音符
+int buttonState;
+int a=0;
+#define Light_value1 400
+int sensorValue;
+#define Light_PIN A2  //光照传感器接AO引脚
+
+#include <SoftwareSerial.h>
+
+#include <Adafruit_NeoPixel.h>
+
+#include <Servo.h>
+
+boolean on_off;
+boolean statusChange;
+bool playing = false;
+
+
+#define Light_value1 400
+
+/*
+  Standalone Sketch to use with a Arduino UNO and a
+  Sharp Optical Dust Sensor GP2Y1010AU0F
+*/
+
+
+
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+#define buzzer_pin 8 //定义蜂鸣器驱动引脚
+#define buzzer_fre 2000 //定义蜂鸣器输出频率
+#define pushButton  6
+
+
+
+
+#include <Adafruit_NeoPixel.h> /*引用”Adafruit_NeoPixel.h”文件。引用的意思有点象“复制-粘贴”。include文件提供了一种很方便的方式共享了很多程序共有的信息。*/
+#define PIN 2                         /*定义了控制LED的引脚，6表示Microduino的D6引脚，可通过Hub转接出来，用户可以更改 */
+#define Light_PIN A2  //光照传感器接AO引脚
+#define Light_value1 400
+
+
+
+
+
+
+void setup(){
+   Serial.begin(9600);
+//   pinMode(ledPower,OUTPUT);
+
+  pinMode(pushButton, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
+  length = sizeof(tune)/sizeof(tune[0]);//这里用了一个sizeof函数，可以查出tone序列里有多少个音符
+
+}
+int k=0;
+
+void loop(){
+
+
+sensorValue = analogRead(Light_PIN);
+if (sensorValue < Light_value1)
+{
+noTone(buzzer_pin);//蜂鸣器不响
+k=0;
+}
+else if (sensorValue >= Light_value1 )
+
+ {if(k==0){
+  for(int x=0;x<length;x++)//循环音符的次数
+  {
+    tone(buzzer_pin,tune[x]);//此函数依次播放tune序列里的数组，即每个音符
+    delay(400*duration[x]);//每个音符持续的时间，即节拍duration，400是调整时间的越大，曲子速度越慢，越小曲子速度越快，自己掌握吧
+    noTone(buzzer_pin);//停止当前音符，进入下一音符
+    
+    if(digitalRead(pushButton)==0) {k=k+1;break;}
+    }
+  }
+ // else if(k==1)
+   // k*=digitalRead(pushButton);
+  }}
